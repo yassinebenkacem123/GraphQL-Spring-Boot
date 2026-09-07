1f9b8949af2da4f3a73231538a4af0afea7a384a
yassinebenkacem123 <yassinbenkacem12@gmail.com>
2026-09-07 20:30:24 +0100

    first commit

---
+++ src/test/java/com/tryit/graphQL/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryit.graphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
